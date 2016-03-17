028c0d7f670455129f99fcffbea6259779ee3df7
Olaf Veerman <olaf.veerman@gmail.com>
2016-03-17 16:55:51 -0400

    Ugly fix to solve dependency loading on production

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -6,6 +6,13 @@ cd dist
 git init
 git config user.name "Travis-CI"
 git config user.email "travis@somewhere.com"
-git add .
+cp ../app/assets/styles/fonts/* assets/fonts
+mkdir node_modules
+mkdir node_modules/rc-slider
+mkdir node_modules/rc-slider/assets
+cp ../node_modules/rc-slider/assets/index.css node_modules/rc-slider/assets
+mkdir node_modules/mapbox.js/
+mkdir node_modules/mapbox.js/theme/
+cp ../node_modules/mapbox.js/theme/style.css node_modules/mapbox.js/theme
 git commit -m "CI deploy to gh-pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
