cd1620e7cc675e7b7e55c0e990cda4bac6165d49
Joe Flasher <joseph.flasher@gmail.com>
2016-03-17 17:34:36 -0400

    Please Travis

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -6,15 +6,12 @@ cd dist
 git init
 git config user.name "Travis-CI"
 git config user.email "travis@somewhere.com"
-mkdir assets/styles/fonts
+mkdir -p assets/styles/fonts
 cp ../app/assets/styles/fonts/* assets/styles/fonts
-mkdir assets/node_modules
-mkdir assets/node_modules/rc-slider
-mkdir assets/node_modules/rc-slider/assets
+mkdir -p assets/node_modules/rc-slider/assets
 cp ../node_modules/rc-slider/assets/index.css assets/node_modules/rc-slider/assets
-mkdir assets/node_modules/mapbox.js/
-mkdir assets/node_modules/mapbox.js/theme/
-cp ../node_modules/mapbox.js/theme/style.css assets/node_modules/mapbox.js/theme
+mkdir -p assets/node_modules/mapbox.js/theme/
+cp ../node_modules/mapbox.js/theme/* assets/node_modules/mapbox.js/theme
 git add .
 git commit -m "CI deploy to gh-pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
