e69f3dec26427c2d0c266d03caa39dce09eff385
Daniel da Silva <daniel@developmentseed.org>
2016-03-18 11:25:10 +0000

    FIx hacks

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -6,12 +6,6 @@ cd dist
 git init
 git config user.name "Travis-CI"
 git config user.email "travis@somewhere.com"
-mkdir -p assets/styles/fonts
-cp ../app/assets/styles/fonts/* assets/styles/fonts
-mkdir -p assets/node_modules/rc-slider/assets
-cp ../node_modules/rc-slider/assets/index.css assets/node_modules/rc-slider/assets
-mkdir -p assets/node_modules/mapbox.js/theme/
-cp -R ../node_modules/mapbox.js/theme/* assets/node_modules/mapbox.js/theme
 git add .
 git commit -m "CI deploy to gh-pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
